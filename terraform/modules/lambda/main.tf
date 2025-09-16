d19cf5748aed763e920bbb19df2c3a9783136746
Moise Germain <60044518+MoiseGermain@users.noreply.github.com>
2025-09-15 21:10:16 -0400

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -30,4 +30,10 @@ resource "aws_lambda_function" "this" {
   runtime       = var.runtime
   role          = aws_iam_role.lambda_role.arn
   source_code_hash = filebase64sha256(var.zip_file_path)
+
+  environment {
+    variables = {
+      SNS_TOPIC_ARN = var.sns_topic_arn
+    }
+  }
 }
