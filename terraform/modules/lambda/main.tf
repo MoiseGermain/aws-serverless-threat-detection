cff3542829329a7c26a784354941f2dbda356cec
Moise Germain <60044518+MoiseGermain@users.noreply.github.com>
2025-09-15 21:01:45 -0400

    Create main.tf

---
+++ terraform/modules/lambda/main.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.function_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "basic_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "sns_publish" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+}
+
+resource "aws_lambda_function" "this" {
+  function_name = var.function_name
+  filename      = var.zip_file_path
+  handler       = var.handler
+  runtime       = var.runtime
+  role          = aws_iam_role.lambda_role.arn
+  source_code_hash = filebase64sha256(var.zip_file_path)
+}
