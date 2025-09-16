de7ce49a795902a9197e8896bff2d1506129dae2
Moise Germain <60044518+MoiseGermain@users.noreply.github.com>
2025-09-15 20:57:23 -0400

    Update variables.tf

--- terraform/modules/cloudtrail/variables.tf
+++ terraform/modules/cloudtrail/variables.tf
@@ -1,35 +1,7 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+variable "trail_name" {
+  type = string
 }
 
-variable "cloudtrail_name" {
-  description = "Name of CloudTrail trail"
-  type        = string
-  default     = "ThreatDetectionTrail"
-}
-
-variable "sns_topic_name" {
-  description = "SNS topic for alerts"
-  type        = string
-  default     = "ThreatAlerts"
-}
-
-variable "lambda_function_name" {
-  description = "Lambda function name"
-  type        = string
-  default     = "ThreatDetectorLambda"
-}
-
-variable "lambda_handler" {
-  description = "Lambda function handler"
-  type        = string
-  default     = "threat_detector.lambda_handler"
-}
-
-variable "lambda_runtime" {
-  description = "Lambda runtime"
-  type        = string
-  default     = "python3.11"
+variable "s3_bucket_name" {
+  type = string
 }
