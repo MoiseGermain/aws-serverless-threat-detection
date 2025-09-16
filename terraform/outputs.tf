3b01f2bd7208caa4b0c00352affb3184faaec03d
Moise Germain <60044518+MoiseGermain@users.noreply.github.com>
2025-09-15 20:58:36 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
-outputs.tf
+output "cloudtrail_name" {
+  value = module.cloudtrail.trail_name
+}
+
+output "sns_topic_arn" {
+  value = module.sns.topic_arn
+}
+
+output "lambda_arn" {
+  value = module.lambda.lambda_arn
+}
+
